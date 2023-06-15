87664015b944a68e8e7aaea04d2b9a11bf0291b3
Aldiyar Serikov <ldr.stlt@gmail.com>
2023-06-16 03:26:08 +0600

    here api setup

--- app/auth/service.py
+++ app/auth/service.py
@@ -3,6 +3,8 @@ from pydantic import BaseSettings
 from app.config import database
 
 from .adapters.jwt_service import JwtService
+from .adapters.here_service import HereService
+from .adapters.s3_service import S3Service
 from .repository.repository import AuthRepository
 
 
@@ -10,6 +12,7 @@ class AuthConfig(BaseSettings):
     JWT_ALG: str = "HS256"
     JWT_SECRET: str = "YOUR_SUPER_SECRET_STRING"
     JWT_EXP: int = 10_800
+    HERE_API_KEY: str
 
 
 config = AuthConfig()
@@ -20,14 +23,20 @@ class Service:
         self,
         repository: AuthRepository,
         jwt_svc: JwtService,
+        s3_svc: S3Service,
+        here_svc: HereService
     ):
         self.repository = repository
         self.jwt_svc = jwt_svc
+        self.s3_service = S3Service()
+        self.here_service = HereService()
 
 
 def get_service():
     repository = AuthRepository(database)
     jwt_svc = JwtService(config.JWT_ALG, config.JWT_SECRET, config.JWT_EXP)
+    s3_service = S3Service()
+    here_service = HereService(config.HERE_API_KEY)
 
-    svc = Service(repository, jwt_svc)
+    svc = Service(repository, jwt_svc, s3_service, here_service)
     return svc
